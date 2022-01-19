e6d7b5716f9337f8b993cdeaa40667668f10ca0a
markuszondler <contact@markuszondler.com>
2022-01-19 21:59:23 +0100

    feature: add ability to define column names in a workspace

--- pages/workspace/[id].vue
+++ pages/workspace/[id].vue
@@ -8,6 +8,7 @@ export default {
     }
   },
   data: () => ({
+    newColumnName: '',
     workspaceName: '',
     board: {
       columns: []
@@ -15,10 +16,14 @@ export default {
   }),
   methods: {
     createColumn() {
-      this.board.columns.push({
-        newItemName: '',
-        items: []
-      })
+      if (this.newColumnName) {
+        this.board.columns.push({
+          columnName: this.newColumnName,
+          newItemName: '',
+          items: []
+        })
+        this.newColumnName = ''
+      }
     },
     createCard(column) {
       if (column.newItemName) {
@@ -42,9 +47,12 @@ export default {
   <main class="workspace-page">
     <h1>{{ workspaceName }} Workspace (#{{ $route.params.id }})</h1>
     <section>
+      <input v-model="newColumnName" @keyup.enter="createColumn" type="text" placeholder="New column title"
+             style="display: block; width: 100%; padding: 5px; font-size: 0.9rem; margin-bottom: 10px;"/>
       <button @click="createColumn" style="margin-bottom: 15px;">Create column</button>
       <div class="column-grid">
         <section class="board-column" v-for="column in board.columns" :key="column">
+          <h3 style="margin-top: 5px;">{{ column.columnName }}</h3>
           <input type="text" v-model="column.newItemName" @keyup.enter="createCard(column)"
                  style="padding: 5px; font-size: 0.9rem;margin-bottom: 10px;" placeholder="New card title">
           <button @click="createCard(column)" style="margin-bottom: 15px;">Create Card</button>
